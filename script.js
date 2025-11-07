674a39250cbc314b09c93d8b908650ceff1a622b
Afia Ofori <oforiafia1@gmail.com>
2025-11-06 16:13:50 -0800

    modified responsives

---
+++ script.js
@@ -0,0 +1,26 @@
+const menuIcon = document.getElementById('menu');
+const navList = document.querySelector('nav ul');
+const navLinks = document.querySelectorAll('nav ul li a');
+
+// Toggle the mobile menu when the hamburger icon is clicked
+menuIcon.addEventListener('click', () => {
+  navList.classList.toggle('active');
+});
+
+// Close the mobile menu when a navigation link is clicked
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navList.classList.remove('active');
+  });
+});
+
+// Show "scroll to top" button when scrolling
+window.addEventListener('scroll', () => {
+  const scrollUp = document.querySelector('.scroll-to-top');
+  // When the scroll is higher than 200 viewport height, add the show-scroll class
+  if (this.scrollY >= 200) {
+    scrollUp.classList.add('show-scroll');
+  } else {
+    scrollUp.classList.remove('show-scroll');
+  }
+});
